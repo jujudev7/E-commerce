ea4c3f81f5a76fe9354cb1adae51e4d07ebd5139
jujudev7 <penna.julien@gmail.com>
2024-05-16 16:54:40 +0200

    ShopCategory & Context added

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div>
-      {/* Contenu du composant ShopCategory */}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Inscription</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Nom" />
+          <input type="email" placeholder="Adresse Email" />
+          <input type="password" placeholder="Mot de passe" />
+        </div>
+        <button>Continuer</button>
+        <p className="loginsignup-login">
+          Déjà un compte ? <span>Connexion</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            En continuant, je suis d'accord avec termes of use privacy policy...
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginSignup;
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import { useContext } from "react";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import { Item } from "../Components/Item/Item";
+import { ShopContext } from "../Context/ShopContext";
+import "./CSS/ShopCategory.css";
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
   return (
-    <div>
-      {/* Contenu du composant ShopCategory */}
+    <div className="shop-category">
+      <img className="shop-category-banner" src={props.banner} alt="" />
+      <div className="shop-category-indexSort">
+        <p>
+          <span>Showing 1-12</span> sur 36 produits
+        </p>
+        <div className="shop-category-sort">
+          Trier par <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shop-category-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shop-category-loadmore">Découvrir +</div>
     </div>
   );
-}
+};
 
 export default ShopCategory;
