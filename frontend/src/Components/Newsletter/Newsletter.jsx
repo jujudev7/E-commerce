40c4f284f47348d375c152e4d28ae6a3716d3f3c
jujudev7 <penna.julien@gmail.com>
2024-05-28 14:54:11 +0200

    Responsive NewCollections.css & Newsletter.css

--- frontend/src/Components/Newsletter/Newsletter.jsx
+++ frontend/src/Components/Newsletter/Newsletter.jsx
@@ -3,7 +3,7 @@ import "./Newsletter.css";
 const Newsletter = () => {
   return (
     <div className="newsletter">
-      <h1>Récupère des Offres Exclusives par Email</h1>
+      <h1>Reçois des Offres Exclusives par Email</h1>
       <p>Abonne-toi à notre newsletter</p>
       <div>
         <input type="email" placeholder="Ton adresse Email" />
