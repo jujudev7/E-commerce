e273645ce997486f054cf87cb685c000c0d1f234
jujudev7 <penna.julien@gmail.com>
2024-06-11 00:15:35 +0200

    MongoDB & API added in index.js

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,21 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect(
+  "mongodb+srv://pennajulien:HouWtI3JrEQBPBMS@cluster0.9zqh9um.mongodb.net/"
+);
+
+// API Creation
+app.get("/", (req, res) => {
+  res.send("Express App is Running");
+});
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server is running on port " + port);
+  } else {
+    console.log("Error : " + error);
+  }
+});
