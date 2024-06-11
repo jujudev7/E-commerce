98f7b3694e534a48d86856fd77a7e9463ceed3cd
jujudev7 <penna.julien@gmail.com>
2024-06-11 23:57:12 +0200

    Image storage added

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,19 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+require("dotenv").config(); // Charger les variables d'environnement
 
 app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-mongoose.connect(
-  "mongodb+srv://pennajulien:HouWtI3JrEQBPBMS@cluster0.9zqh9um.mongodb.net/"
-);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
 
 // API Creation
 app.get("/", (req, res) => {
@@ -26,4 +31,28 @@ app.listen(port, (error) => {
   } else {
     console.log("Error : " + error);
   }
+});
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for images
+
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
 });
