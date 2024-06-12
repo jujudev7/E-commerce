c50cc3a10b3e8b37cefb61f0ef7da7fd6ef10c10
jujudev7 <penna.julien@gmail.com>
2024-06-12 17:56:29 +0200

    Schema Creating Products added + env.local

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-require("dotenv").config(); // Charger les variables d'environnement
+require("dotenv").config({ path: "./.env.local" }); // Charger les variables d'environnement
 
 app.use(express.json());
 app.use(cors());
@@ -54,5 +54,60 @@ app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
     image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post("/addproduct", async (req, res) => {
+  const product = new Product({
+    id: req.body.id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  res.json({
+    success: true,
+    name: req.body.name,
   });
 });
